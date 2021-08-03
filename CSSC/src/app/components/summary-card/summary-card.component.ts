005bf0f284f66bbe77598b28bd804407222831b4
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-03 14:40:08 +0800

    dashboard

--- CSSC/src/app/components/summary-card/summary-card.component.ts
+++ CSSC/src/app/components/summary-card/summary-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-summary-card',
-  templateUrl: './summary-card.component.html',
-  styleUrls: ['./summary-card.component.scss']
+	selector: 'app-summary-card',
+	templateUrl: './summary-card.component.html',
+	styleUrls: ['./summary-card.component.scss'],
 })
 export class SummaryCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor() {}
+	@Input() title: string = ''
+	@Input() value: number = 0
+	@Input() class: string = ''
+	ngOnInit(): void {}
 }
