e5ba0244751819ece33db57b81f0514323ace083
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-04 21:20:31 +0800

    voter login

--- CSSC/src/app/pages/landing/compoents/voter-login/voter-login.component.ts
+++ CSSC/src/app/pages/landing/compoents/voter-login/voter-login.component.ts
@@ -1,5 +1,10 @@
+import { Voter } from './../../../../Models/User'
+import { Collections } from './../../../../Models/Admin'
+import { AngularFirestore } from '@angular/fire/firestore'
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
+import { Alert, Welcome } from 'src/app/components/Alert'
+import { ModalService } from 'src/app/services/modal.service'
 
 @Component({
 	selector: 'app-voter-login',
@@ -7,11 +12,43 @@ import { Router } from '@angular/router'
 	styleUrls: ['./voter-login.component.scss'],
 })
 export class VoterLoginComponent implements OnInit {
-	constructor(private router: Router) {}
+	constructor(
+		private router: Router,
+		private firestore: AngularFirestore,
+		private modal: ModalService
+	) {}
 
 	ngOnInit(): void {}
 
+	id_number: string = ''
+
 	login() {
-		this.router.navigate(['home/vote'])
+		if (this.id_number === '') {
+			Alert('Error', 'One or more fields should not be empty', 'error')
+			return
+		}
+		this.firestore
+			.collection(Collections.Voters, (ref) =>
+				ref.where('id_number', '==', this.id_number)
+			)
+			.snapshotChanges()
+			.subscribe((data: any[]) => {
+				if (data.length === 0) {
+					Alert(
+						'Sorry',
+						'We could not find a student associated with this ID Number',
+						'error'
+					)
+					return
+				}
+				let user: Voter = data[0]
+				localStorage.setItem('role', 'voter')
+				localStorage.setItem('user', JSON.stringify(user))
+				Welcome(
+					`${user.name} ${user.campus} ${user.course} of ${user.department} Department`
+				)
+				this.router.navigate(['home/vote'])
+				this.modal.close()
+			})
 	}
 }
