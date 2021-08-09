c212875d94eead1a98480d8a8df0a29bc2480e0a
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-09 21:40:27 +0800

    dashboard

--- CSSC/src/app/components/table/table.component.ts
+++ CSSC/src/app/components/table/table.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Collections } from 'src/app/Models/Admin'
+import { Component, Input, OnInit } from '@angular/core'
+import { BaseService } from 'src/app/services/base.service'
+import { Candidate } from 'src/app/Models/Candidtate'
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+	selector: 'app-table',
+	templateUrl: './table.component.html',
+	styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
+	constructor(private service: BaseService) {}
+	@Input() header: string = ''
+	@Input() voters: number = 0
 
-  constructor() { }
+	ngOnInit(): void {
+		setTimeout(() => {
+			this.getCandidates()
+		}, 500)
+	}
 
-  ngOnInit(): void {
-  }
+	candidates: any = []
 
+	getCandidates() {
+		this.service.firestore
+			.collection(Collections.Candidate, (ref) => ref.where('partylist', '==', this.header))
+			.valueChanges({ idField: 'id' })
+			.subscribe((data: any) => {
+				this.candidates = data
+				this.processVotes()
+			})
+	}
+
+	processVotes() {
+		this.service.firestore
+			.collection(Collections.Votes)
+			.valueChanges()
+			.subscribe((data: any) => {
+				const votes: VoteType[] = data
+				for (let index in this.candidates) {
+					this.candidates[index].votes = 0
+					votes.forEach((vote: VoteType) => {
+						vote.bets.forEach((candidate_id: string) => {
+							if (candidate_id === this.candidates[index].id) {
+								this.candidates[index].votes += 1
+							}
+						})
+					})
+				}
+			})
+	}
+
+	percentOf(x: number | any, y: number | any) {
+		return (parseFloat(x) * 100) / parseFloat(y)
+	}
+}
+
+type VoteType = {
+	bets: any[]
+	voter: string
 }
--- CSSC/src/app/pages/home/dashboard/dashboard.component.ts
+++ CSSC/src/app/pages/home/dashboard/dashboard.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Collections } from './../../../Models/Admin'
+import { BaseService } from './../../../services/base.service'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+	constructor(private service: BaseService) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.getPartylists()
+		this.getCandidates()
+		this.getVoters()
+		this.getVoted()
+	}
 
-  ngOnInit(): void {
-  }
+	partylists: number = 0
+	candidates: number = 0
+	voters: number = 0
+	voted: number = 0
 
+	partylists_data: any = []
+
+	getPartylists() {
+		new BaseService(this.service.firestore, Collections.Partylist, [], this.service.store).fetchAll().subscribe((data: any) => {
+			this.partylists = data.length
+			this.partylists_data = data
+		})
+	}
+
+	getCandidates() {
+		this.service.firestore
+			.collection(Collections.Candidate)
+			.valueChanges()
+			.subscribe((data: any) => {
+				this.candidates = data.length
+			})
+	}
+
+	getVoters() {
+		new BaseService(this.service.firestore, Collections.Voters, [], this.service.store).fetchAll().subscribe((data: any) => {
+			this.voters = data.length
+		})
+	}
+
+	getVoted() {
+		this.service.firestore
+			.collection(Collections.Votes)
+			.valueChanges()
+			.subscribe((data: any) => {
+				this.voted = data.length
+			})
+	}
 }
