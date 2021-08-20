5f3b8c9b8fa7dfafa0c38a3b19aab7ce37717484
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-20 10:34:07 +0800

    KeyBoard Avoiding View

--- Mobile/screens/sign-in/Sign-in.tsx
+++ Mobile/screens/sign-in/Sign-in.tsx
@@ -1,7 +1,19 @@
 
 import { useNavigation } from '@react-navigation/native';
 import React, { FC } from 'react';
-import { Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+    Dimensions,
+    Image,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Platform,
+    Keyboard
+} from 'react-native';
 import Container from '../../constants/Layout';
 import { collection } from '../../firebase/firebase';
 import { Collections } from '../../Models/Admin';
@@ -48,41 +60,47 @@ const SignIn: FC<Props> = ( props ) => {
 
     return (
         <Container>
-            <Image style={style.image} source={require( '../../assets/logo/trans-logo.png' )} />
-            <Text style={style.title}>Cast your vote on your favorite electoral candidate!</Text>
-            <Text style={{ textAlign: 'center' }}>Vote Wisely! <Text style={{ color: 'orange' }}>College Supreme Student Council</Text></Text>
-            <View style={{ padding: 35 }}>
+            <KeyboardAvoidingView
+                keyboardVerticalOffset={-150}
+                behavior={Platform.OS == 'ios' ? 'position' : 'height'}
+                style={{ flex: 1 }}
+            >
+                <Image style={style.image} source={require( '../../assets/landing/bg1.png' )} />
+                <Text style={style.title}>Cast your vote on your favorite electoral candidate!</Text>
+                <Text style={{ textAlign: 'center' }}>Vote Wisely! <Text style={{ color: 'orange' }}>College Supreme Student Council</Text></Text>
+                <View style={{ padding: 35 }}>
+                    <TextInput
+                        clearButtonMode="always"
+                        placeholderTextColor="orange"
+                        value={IDnumber}
+                        onChangeText={( text: string ) => setIDnumber( text.toUpperCase() )}
+                        style={style.input} placeholder="ID Number" />
 
-                <TextInput
-                    clearButtonMode="always"
-                    placeholderTextColor="orange"
-                    onChangeText={( text: string ) => setIDnumber( text )}
-                    style={style.input} placeholder="ID Number" />
+                    <TextInput
+                        clearButtonMode="always"
+                        placeholderTextColor="orange"
+                        secureTextEntry={true}
+                        value={Section}
+                        onChangeText={( text: string ) => setSection( text.toUpperCase() )}
+                        style={style.input} placeholder="Section" />
 
-                <TextInput
-                    clearButtonMode="always"
-                    placeholderTextColor="orange"
-                    secureTextEntry={true}
-                    onChangeText={( text: string ) => setSection( text )}
-                    style={style.input} placeholder="Section" />
-
-                <Text style={{ color: 'red', textAlign: 'center', marginBottom: 20 }}>
-                    {
-                        hasError ? 'One or more fields should not be empty!' : ''
-                    }
-                </Text>
-
-                <TouchableOpacity disabled={isLoading}
-                    onPress={() => voteNow()}
-                    style={[ style.button, { borderRadius: 30 } ]}>
-                    <Text style={[ style.title, { padding: 5 } ]}>
+                    <Text style={{ color: 'red', textAlign: 'center', marginBottom: 10 }}>
                         {
-                            isLoading ? 'please wait..' : 'Vote Now!'
+                            hasError ? 'One or more fields should not be empty!' : ''
                         }
                     </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity disabled={isLoading}
+                        onPress={() => voteNow()}
+                        style={[ style.button, { borderRadius: 30 } ]}>
+                        <Text style={[ style.title, { padding: 5, color: 'white' } ]}>
+                            {
+                                isLoading ? 'please wait..' : 'Vote Now!'
+                            }
+                        </Text>
+                    </TouchableOpacity>
+                </View>
 
-            </View>
+            </KeyboardAvoidingView>
             <Text style={{ position: 'absolute', bottom: 20, textAlign: 'center', width: '100%' }}>All Rights Reserved: Powered By: <Text style={{ color: 'orange' }}>Jamel Eid Yassin</Text></Text>
         </Container>
     );
@@ -90,21 +108,21 @@ const SignIn: FC<Props> = ( props ) => {
 
 const style = StyleSheet.create( {
     image: {
-        height: Dimensions.get( 'screen' ).height / 5.5,
+        height: Dimensions.get( 'screen' ).height / 3.5,
         resizeMode: 'contain',
         width: Dimensions.get( 'screen' ).width,
     },
     title: {
         fontWeight: 'bold',
         textAlign: 'center',
-        fontSize: 30,
-        margin: 10,
+        fontSize: 20,
+        margin: 5,
         padding: 20
     },
     input: {
         borderRadius: 30,
         borderWidth: 1,
-        padding: 16,
+        padding: 10,
         marginBottom: 16,
         borderColor: 'rgba(150,150,150,.2)',
         fontWeight: 'bold',
